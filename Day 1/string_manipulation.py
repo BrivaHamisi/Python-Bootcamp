cdb0b6db06643e43f9337132fb98cced9637a648
BrivaHamisi <brivahamisi@gmail.com>
2025-07-12 07:17:35 +0300

    String Concatenation and Input Functions

--- Day 1/string_manipulation.py
+++ Day 1/string_manipulation.py
@@ -1,3 +1,7 @@
-print("Hello World!\nThis is my first Python code.")
+# print("Hello World!\nThis is my first Python code.")
+# print("Hello" + " " + "Angela")
 
-print("Hello" + "Angela")
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.' )
+print('e.g. print("Hello" + "World")')
+print("New lines can be created with a backslash and an n.")
