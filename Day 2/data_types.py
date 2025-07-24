f4441c8d1c8e6b22b5ba4c41465a0d2315d1b8b0
BrivaHamisi <brivahamisi@gmail.com>
2025-07-24 19:03:52 +0300

    Data types

--- Day 2/data_types.py
+++ Day 2/data_types.py
@@ -1,4 +1,25 @@
 #Data Types
 #Strings
 
-print("Hello"[0])
+print("Hello"[0]) #Also known as subscripting
+
+print("123"+"456") #Concatenation
+
+
+#Integer
+print(123+456)
+
+#Float
+print(3.14159)
+
+#Boolean
+print(True)
+print(False)
+
+#Type Conversion
+print(type(123))  # <class 'int'>
+print(type(123.0)) # <class 'float'>
+print(type(True)) # <class 'bool'>
+print(type("Hello")) # <class 'str'>
+
+#Type Casting
