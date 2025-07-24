ecb415c6b6f5dec22cda9777f59b63020332c1ed
BrivaHamisi <brivahamisi@gmail.com>
2025-07-24 18:00:31 +0300

    Variables Exercise

---
+++ Day 1/variables_exercise.py
@@ -0,0 +1,10 @@
+a = input("a: ")
+b = input("b: ")
+
+print("a: " + a)
+print("b: " + b)
+
+a,b = b,a
+
+print("The Value of a has been changed to: " + a)
+print("The value of b has been changed to: " + b)
