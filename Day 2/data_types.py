2b45dec112f6fc68e20b0e632024f49b2ded1e23
BrivaHamisi <brivahamisi@gmail.com>
2025-08-09 08:20:41 +0300

    Data Types

--- Day 2/data_types.py
+++ Day 2/data_types.py
@@ -2,8 +2,11 @@
 #Strings
 
 print("Hello"[0]) #Also known as subscripting
+print("Hello"[4]) #Print the last 0
 
 print("123"+"456") #Concatenation
+
+123_456_678 #Python ignores the underescores, for visual purposes
 
 
 #Integer
