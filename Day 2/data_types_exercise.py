f4280bff69edd231d70424124c25bdab62b3606d
BrivaHamisi <brivahamisi@gmail.com>
2025-08-11 10:26:38 +0300

    Data Types Status

---
+++ Day 2/data_types_exercise.py
@@ -0,0 +1,9 @@
+two_digit = str(print(input("Type a Two digit number:")))
+
+print(two_digit)
+print(two_digit[0])
+
+# num_1 = int(two_digit[0])
+# num_2 = int(two_digit[1])
+
+# print(num_1, num_2)
